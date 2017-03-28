a4538c5d8e99900ca87c77ccddf173dee7713688
andycarmona <andres.carmona@uni-app.se>
2017-03-28 16:52:55 +0200

    [] Refactor map request . chain

--- app/components/chart/areaChart.js
+++ app/components/chart/areaChart.js
@@ -65,13 +65,13 @@ function Controller($q) {
             vAxes: {
                 series: {
                     0: { pointSize: 5 },
-                    2: { }
+                    2: {}
                 }
             },
             series: {
                 0: { pointsVisible: false },
-                1: {lineDashStyle: dashedLineStyle, pointsVisible: false, areaOpacity: 0.5 },
-                2: { pointsVisible: false,lineDashStyle: dashedLineStyle, pointsVisible: false },
+                1: { lineDashStyle: dashedLineStyle, pointsVisible: false, areaOpacity: 0.5 },
+                2: { pointsVisible: false, lineDashStyle: dashedLineStyle, pointsVisible: false },
                 3: {
                     pointsVisible: false,
                     lineDashStyle: dashedLineStyle,
@@ -87,7 +87,7 @@ function Controller($q) {
                     pointsVisible: false, areaOpacity: 0,
                     visibleInLegend: false, color: 'black'
                 },
-                 6: {
+                6: {
                     pointsVisible: false, areaOpacity: 0,
                     visibleInLegend: false, color: 'black'
                 },
@@ -149,7 +149,9 @@ function Controller($q) {
     function getVehicleUtilizationData() {
         let rawData = { 'totalDistance': 0, 'totalDuration': 0, 'totalFuelUsed': 0, 'totalEcoScore': 0, 'utilizationIntervals': [{ 'distance': 756, 'averageSpeed': 55, 'averageDistance': 756, 'averageFuelConsumption': 14, 'averageTotalEcoScore': 78, 'intervalStartDate': '2017-03-02', 'intervalEndDate': '2017-03-02', 'nrOfVehicles': 61, 'nrOfDrivers': 61 }, { 'distance': 446, 'averageSpeed': 98, 'averageDistance': 446, 'averageFuelConsumption': 12, 'averageTotalEcoScore': 31, 'intervalStartDate': '2017-03-03', 'intervalEndDate': '2017-03-03', 'nrOfVehicles': 61, 'nrOfDrivers': 61 }, { 'distance': 844, 'averageSpeed': 55, 'averageDistance': 844, 'averageFuelConsumption': 10, 'averageTotalEcoScore': 76, 'intervalStartDate': '2017-03-04', 'intervalEndDate': '2017-03-04', 'nrOfVehicles': 61, 'nrOfDrivers': 61 }, { 'distance': 233, 'averageSpeed': 64, 'averageDistance': 233, 'averageFuelConsumption': 6, 'averageTotalEcoScore': 95, 'intervalStartDate': '2017-03-05', 'intervalEndDate': '2017-03-05', 'nrOfVehicles': 61, 'nrOfDrivers': 61 }, { 'distance': 863, 'averageSpeed': 54, 'averageDistance': 863, 'averageFuelConsumption': 7, 'averageTotalEcoScore': 28, 'intervalStartDate': '2017-03-06', 'intervalEndDate': '2017-03-06', 'nrOfVehicles': 61, 'nrOfDrivers': 61 }, { 'distance': 757, 'averageSpeed': 79, 'averageDistance': 757, 'averageFuelConsumption': 9, 'averageTotalEcoScore': 57, 'intervalStartDate': '2017-03-07', 'intervalEndDate': '2017-03-07', 'nrOfVehicles': 61, 'nrOfDrivers': 61 }] }
         let mileageHolder = [];
-        let actualPassed = false;
+        let index = 1;
+        let statusPassed = false;
+        let todaysPassed = false;
 
         let result = rawData.utilizationIntervals
             .map((element, index) => {
@@ -160,26 +162,27 @@ function Controller($q) {
                     intervalStartDate: element.intervalStartDate,
                     status: element.distance >= userData.contractMileage ? 0 : 1
                 };
-            })
-        //result = setOverdueMileage(result);
+            }).map((el, i) => {
+                let serie = [
+                    getRowsDates(el.intervalStartDate),
+                    { v: null },
+                    { v: null },
+                    { v: null },
+                    { v: null },
+                    { v: userData.contractMileage }]
 
-        let index = 1;
-        let statusPassed = false;
-        let todaysPassed = false;
-        let finalResult = result.map((el, i) => {
-            let serie = [getRowsDates(el.intervalStartDate), { v: null }, { v: null }, { v: null }, { v: null }, { v: userData.contractMileage }]
-            setColor(0, 'success');
-            if ((el.distance >= userData.contractMileage) && (!statusPassed)) {
-                statusPassed = true;
-                index++;
-                setColor(index, 'alert');
-                serie[index - 1] = { v: el.distance };
+                setColor(0, 'success');
+                if ((el.distance >= userData.contractMileage) && (!statusPassed)) {
+                    statusPassed = true;
+                    index++;
+                    setColor(index, 'alert');
+                    serie[index - 1] = { v: el.distance };
 
-            }
-            serie[index] = { v: el.distance };
-            return serie;
-        });
-        return finalResult;
+                }
+                serie[index] = { v: el.distance };
+                return serie;
+            });
+        return result;
     }
 
     function setColor(index, state) {
